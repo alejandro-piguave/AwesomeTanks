3d66fc2d34a298215658ba15c9b8bf12280e4a6b
Alejandro Piguave <alejandro.piguave.illescas@gmail.com>
2024-09-20 11:06:28 +0200

    rename TextureParticleProjectileGraphicsComponent add ParticleProjectileGraphicsComponent

--- core/src/com/alexpi/awesometanks/game/projectiles/components/graphics/TextureParticleProjectileGraphicsComponent.kt
+++ core/src/com/alexpi/awesometanks/game/projectiles/components/graphics/TextureParticleProjectileGraphicsComponent.kt
@@ -2,11 +2,14 @@ package com.alexpi.awesometanks.game.projectiles.components.graphics
 
 import com.alexpi.awesometanks.game.projectiles.Projectile
 import com.alexpi.awesometanks.screens.game.stage.GameContext
+import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.ParticleEffect
+import com.badlogic.gdx.graphics.g2d.TextureRegion
 
-class ParticleProjectileGraphicsComponent(gameContext: GameContext, particlePath: String): ProjectileGraphicsComponent {
+class TextureParticleProjectileGraphicsComponent(gameContext: GameContext, texturePath: String, particlePath: String): ProjectileGraphicsComponent {
 
+    private val textureRegion: TextureRegion = TextureRegion(gameContext.getAssetManager().get<Texture>(texturePath))
     private val particleEffect: ParticleEffect = ParticleEffect(gameContext.getAssetManager().get(particlePath))
     override fun update(projectile: Projectile, delta: Float) {
         particleEffect.setPosition(projectile.x + projectile.width/2, projectile.y + projectile.height/2)
@@ -15,6 +18,9 @@ class ParticleProjectileGraphicsComponent(gameContext: GameContext, particlePath
     }
 
     override fun draw(projectile: Projectile, batch: Batch, parentAlpha: Float) {
-        particleEffect.draw(batch)
+        with(projectile) {
+            batch.draw(textureRegion, x, y, originX, originY, width, height, scaleX, scaleY, rotation)
+            particleEffect.draw(batch)
+        }
     }
 }
